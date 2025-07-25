fd918aed778217f54e2fd1ef6548f975ba69c38d
Haikal Aufan <aufan2002@gmail.com>
2025-07-25 22:10:18 +0700

    Update Project

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -1,5 +1,14 @@
 export const projects = [
   {
+    title: "Portfolio Website",
+    description:
+      "My personal portfolio website showcasing my projects and skills.",
+    technologies: ["Next.js", "Tailwind CSS", "Framer Motion"],
+    githubLink: "https://github.com/L4Fauna/my-portofolio",
+    demoLink: "https://lafauna-porto.vercel.app/",
+    image: "/projects/myporto-thumbnail.jpg",
+  },
+  {
     title: "ReactJs Contact App",
     description:
       "A contact app built with React.Js",
@@ -9,72 +18,73 @@ export const projects = [
     image: "/projects/contact-app-thumbnail.png",
   },
   {
-    title: "E-commerce Platform",
+    title: "Javascript TODO App",
     description:
-      "A full-stack e-commerce platform built with Next.js, TypeScript, and Tailwind CSS.",
-    technologies: ["Next.js", "TypeScript", "Tailwind CSS", "Stripe"],
-    githubLink: "https://github.com",
-    demoLink: "https://demo.com",
-    image: "/projects/e-commerce-website.png",
+      "A contact app built with Javascript",
+    technologies: ["Javascript"],
+    githubLink: "https://github.com/L4Fauna/todoapps",
+    demoLink: "https://todoapps-three.vercel.app/",
+    image: "/projects/todo-app-thumbnail.png",
   },
-  {
-    title: "Task Management App",
-    description:
-      "A collaborative task management application with real-time updates.",
-    technologies: ["React", "Node.js", "MongoDB", "Socket.io"],
-    githubLink: "https://github.com",
-    demoLink: "https://demo.com",
-    image: "/projects/task-manager.webp",
-  },
-  {
-    title: "Portfolio Website",
-    description:
-      "My personal portfolio website showcasing my projects and skills.",
-    technologies: ["Next.js", "Tailwind CSS", "Framer Motion"],
-    githubLink: "https://github.com",
-    demoLink: "https://demo.com",
-    image: "/projects/portfolio-website.jpg",
-  },
-  {
-    title: "Blog Platform",
-    description:
-      "A blogging platform with user authentication and markdown support.",
-    technologies: ["Gatsby", "GraphQL", "Contentful"],
-    githubLink: "https://github.com",
-    demoLink: "https://demo.com",
-    image: "/projects/blog-website.jpeg",
-  },
-  {
-    title: "Weather App",
-    description:
-      "A weather application that provides real-time weather updates.",
-    technologies: ["React", "OpenWeatherMap API"],
-    githubLink: "https://github.com",
-    demoLink: "https://demo.com",
-    image: "/projects/weather-app.png",
-  },
-  {
-    title: "Chat Application",
-    description: "A real-time chat application using WebSocket technology.",
-    technologies: ["React", "Node.js", "Socket.io"],
-    githubLink: "https://github.com",
-    demoLink: "https://demo.com",
-    image: "/projects/chat-app.png",
-  },
-  {
-    title: "Recipe Finder",
-    description: "A recipe search application using the Edamam API.",
-    technologies: ["React", "Edamam API"],
-    githubLink: "https://github.com",
-    demoLink: "https://demo.com",
-    image: "/projects/recipe-finder.png",
-  },
-  {
-    title: "Expense Tracker",
-    description: "A personal finance tracker to manage expenses and income.",
-    technologies: ["React", "Firebase"],
-    githubLink: "https://github.com",
-    demoLink: "https://demo.com",
-    image: "/projects/expense-tracker.webp",
-  },
+  
+  // {
+  //   title: "E-commerce Platform",
+  //   description:
+  //     "A full-stack e-commerce platform built with Next.js, TypeScript, and Tailwind CSS.",
+  //   technologies: ["Next.js", "TypeScript", "Tailwind CSS", "Stripe"],
+  //   githubLink: "https://github.com",
+  //   demoLink: "https://demo.com",
+  //   image: "/projects/e-commerce-website.png",
+  // },
+  // {
+  //   title: "Task Management App",
+  //   description:
+  //     "A collaborative task management application with real-time updates.",
+  //   technologies: ["React", "Node.js", "MongoDB", "Socket.io"],
+  //   githubLink: "https://github.com",
+  //   demoLink: "https://demo.com",
+  //   image: "/projects/task-manager.webp",
+  // },
+  // {
+  //   title: "Blog Platform",
+  //   description:
+  //     "A blogging platform with user authentication and markdown support.",
+  //   technologies: ["Gatsby", "GraphQL", "Contentful"],
+  //   githubLink: "https://github.com",
+  //   demoLink: "https://demo.com",
+  //   image: "/projects/blog-website.jpeg",
+  // },
+  // {
+  //   title: "Weather App",
+  //   description:
+  //     "A weather application that provides real-time weather updates.",
+  //   technologies: ["React", "OpenWeatherMap API"],
+  //   githubLink: "https://github.com",
+  //   demoLink: "https://demo.com",
+  //   image: "/projects/weather-app.png",
+  // },
+  // {
+  //   title: "Chat Application",
+  //   description: "A real-time chat application using WebSocket technology.",
+  //   technologies: ["React", "Node.js", "Socket.io"],
+  //   githubLink: "https://github.com",
+  //   demoLink: "https://demo.com",
+  //   image: "/projects/chat-app.png",
+  // },
+  // {
+  //   title: "Recipe Finder",
+  //   description: "A recipe search application using the Edamam API.",
+  //   technologies: ["React", "Edamam API"],
+  //   githubLink: "https://github.com",
+  //   demoLink: "https://demo.com",
+  //   image: "/projects/recipe-finder.png",
+  // },
+  // {
+  //   title: "Expense Tracker",
+  //   description: "A personal finance tracker to manage expenses and income.",
+  //   technologies: ["React", "Firebase"],
+  //   githubLink: "https://github.com",
+  //   demoLink: "https://demo.com",
+  //   image: "/projects/expense-tracker.webp",
+  // },
 ];
